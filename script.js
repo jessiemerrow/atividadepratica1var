60156e1ee298d7dd1f06be253bb143b089895d8c
Jana ( JessieMerrow) <41707901+jessiemerrow@users.noreply.github.com>
2022-04-07 21:30:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*var sobrenome = "sobrenome";
+var idade = "idade";
+/*var elNumero = +(document.getElementById('minumero').innerHTML; /
+/*document.write("Meu nome éJanaína Serrano e tenho 44 anos")*/
+
+/*var fisrtname = document.getElementById('nome').innerHTML; */
+/*document.write()*/
+
+var firstName = document.getElementById('nome').innerHTML;
+var lastName = document.getElementById('sobrenome').innerHTML;
+var age = document.getElementById('idade').innerHTML;
+
+document.write("Meu nome é " + firstName + " " + lastName + " e tenho " + age);
